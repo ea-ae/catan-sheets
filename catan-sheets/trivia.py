e6ae765576c904b3da550ff7a3d3d7d0d6bf0c1c
aeon <aeon@fakeemail.com>
2025-01-05 17:51:43 +0200

    make trivia generic

--- catan-sheets/trivia.py
+++ catan-sheets/trivia.py
@@ -1,86 +1,79 @@
 from dataclasses import dataclass
-from typing import Any, Callable
+from typing import Any, Callable, TypeVar, Generic
 import math
 
 
-# @dataclass
-# class TriviaData:
-#     color_to_name: dict[int, str]
-#     json: dict[str, Any]
+T = TypeVar("T")
 
 
 @dataclass
-class Trivia:
-    f: Callable[[str, dict[str, Any]], float]
-    description: Callable[[str, float], str]
-    fun_factor: Callable[[float], float]
+class Trivia(Generic[T]):
+    f: Callable[[str, dict[str, Any]], T]
+    description: Callable[[str, T], str]
+    fun_factor: Callable[[T], float]
 
 
 TRIVIAS = [
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["resourceStats"][p]["robbingLoss"],
         description=lambda name, x: f"{name} got robbed {x} times",
-        fun_factor=lambda x: x * 0.7
+        fun_factor=lambda x: x * 0.7,
     ),
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["resourceStats"][p]["robbingIncome"],
         description=lambda name, x: f"{name} robbed others {x} times",
-        fun_factor=lambda x: x * 0.5
+        fun_factor=lambda x: x * 0.5,
     ),
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["resourceStats"][p]["rollingLoss"],
         description=lambda name, x: f"{name} lost {x} resources by getting 7'd out",
-        fun_factor=lambda x: x * 0.35
+        fun_factor=lambda x: x * 0.35,
     ),
-
-    Trivia(
-        f=lambda p, json: to_stats(json)["resourceStats"][p]["tradeIncome"] - to_stats(json)["resourceStats"][p]["tradeLoss"],
+    Trivia[float](
+        f=lambda p, json: to_stats(json)["resourceStats"][p]["tradeIncome"]
+        - to_stats(json)["resourceStats"][p]["tradeLoss"],
         description=lambda name, x: f"{name} traded a net profit of {x} cards",
-        fun_factor=lambda x: x * 2
+        fun_factor=lambda x: x * 2,
     ),
-
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["activityStats"][p]["resourceIncomeBlocked"],
         description=lambda name, x: f"{name} lost {x} resources to blocked tiles",
-        fun_factor=lambda x: x * 0.2
+        fun_factor=lambda x: x * 0.2,
     ),
-
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["diceStats"][0],
         description=lambda name, x: f"Two was rolled {x} times this game",
-        fun_factor=lambda x: x * 1.5
+        fun_factor=lambda x: x * 1.5,
     ),
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["diceStats"][1],
         description=lambda name, x: f"Three was rolled {x} times this game",
-        fun_factor=lambda x: x * 1
+        fun_factor=lambda x: x * 1,
     ),
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["diceStats"][9],
         description=lambda name, x: f"Eleven was rolled {x} times this game",
-        fun_factor=lambda x: x * 1
+        fun_factor=lambda x: x * 1,
     ),
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["diceStats"][10],
         description=lambda name, x: f"Twelve was rolled {x} times this game",
-        fun_factor=lambda x: x * 1.5
+        fun_factor=lambda x: x * 1.5,
     ),
-
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["gameDurationInMS"] // 60_000,
         description=lambda name, x: f"This game lasted only {x} minutes",
-        fun_factor=lambda x: 23 - x
+        fun_factor=lambda x: 23 - x,
     ),
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["totalTurnCount"],
         description=lambda name, x: f"This game lasted only {x} turns",
-        fun_factor=lambda x: 55 - x
+        fun_factor=lambda x: 55 - x,
     ),
-
-    Trivia(
+    Trivia[float](
         f=lambda p, json: to_stats(json)["players"][p]["victoryPoints"].get("2", 0),
         description=lambda name, x: f"{name} bought {x} VP devs",
-        fun_factor=lambda x: x * 10 if x >=4 else 0
+        fun_factor=lambda x: x * 10 if x >= 4 else 0,
     ),
 ]
 
@@ -102,7 +95,7 @@ def generate_trivia(json: dict[str, Any]):
                 player_name = color_to_name[player_color]
                 best_trivia = trivia.description(player_name, score)
                 best_fun_factor = fun_factor
-    
+
     return best_trivia
 
 
